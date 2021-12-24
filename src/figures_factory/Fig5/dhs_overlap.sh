dc696746653a1f6d552721dc3761524075e9716e
Surag <10693069+suragnair@users.noreply.github.com>
2021-12-24 16:25:00 +0000

    DHS overlap analysis

--- src/figures_factory/Fig5/dhs_overlap.sh
+++ src/figures_factory/Fig5/dhs_overlap.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 set -u 
 
-BASE=/users/surag/kundajelab/scATAC-reprog/src/analysis/20200307_fine_clustering/beds/20201211_gridmap_peakwidthnorm_logplusznorm_4way_ordered_n20/width_200bp 
+BASE=/users/surag/kundajelab/scATAC-reprog/src/analysis/20200307_fine_clustering/beds/20210719_gridmap_peakwidthnorm_logplusznorm_4way_assited_n20 
 DHS=/users/surag/kundajelab/scATAC-reprog/resources/DHSIndex/DHS_Index_and_Vocabulary_hg38_WM20190703.txt.gz
 
 # check DHS sorted
@@ -11,11 +11,9 @@ zcat $DHS | sed 1d | sort -c -k1,1 -k2,2n
 
 for i in {1..20} 
   do
-    # verify sorted
-    cat $BASE/idx$i.bed | sort -c -k1,1 -k2,2n
-
     a=$(cat  $BASE/idx$i.bed | wc -l ) 
-    b=$(bedtools coverage -a $BASE/idx$i.bed -b <(zcat $DHS | sed 1d) -sorted | awk '$5>50' | wc -l )
+    b=$(bedtools coverage -a <(cat $BASE/idx$i.bed | sort -k1,1 -k2,2n) -b <(zcat $DHS | sed 1d) -sorted | awk '$5>100' | wc -l )
+#    b=$(bedtools coverage -a <(cat $BASE/idx$i.bed | sort -k1,1 -k2,2n) -b <(zcat $DHS | sed 1d) -sorted | awk '{s+=$5/($3-$2)} END{print s}')
     c=$(printf "$b/$a\n" | bc -l) 
     printf "$i\t$c\t$b\t$a\n"
 done
